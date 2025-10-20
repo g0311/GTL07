19530f3ac561f28cb3d61fb442a730f80ead4d80
Sunha-i <icecream20@khu.ac.kr>
2025-10-21 07:30:54 +0900

    Chore: add light description

--- Engine/Source/Component/Light/Public/DirectionalLightComponent.h
+++ Engine/Source/Component/Light/Public/DirectionalLightComponent.h
@@ -1,6 +1,16 @@
 #pragma once
 #include "Component/Light/Public/LightComponent.h"
 
+/**
+ * UDirectionalLightComponent
+ *
+ * Represents a directional light source (like sunlight) that emits parallel rays
+ * across the entire scene. It has no position, only a direction and color intensity.
+ *
+ * Key Property:
+ *		- Constant illumination direction for all fragments.
+ *      - No attenuation with distance.
+ */
 UCLASS()
 class UDirectionalLightComponent : public ULightComponent
 {
--- Engine/Source/Component/Light/Public/PointLightComponent.h
+++ Engine/Source/Component/Light/Public/PointLightComponent.h
@@ -1,6 +1,16 @@
 #pragma once
 #include "Component/Light/Public/LightComponent.h"
 
+/**
+ * UPointLightComponent
+ *
+ * Represents a point light source that emits light uniformly in all directions
+ * from a single position in world space. It has both a position and distance-based attenuation.
+ *
+ * Key Property:
+ *		- Emits light spherically from its position.
+ *      - It attenuates over distance, with the rate of decay controlled by the FalloffExponent.
+ */
 UCLASS()
 class UPointLightComponent : public ULightComponent
 {
